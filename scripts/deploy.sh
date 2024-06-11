f752c429fbc7a79879f8cc62e59a9f00698c540b
Cristina Rodriguez <yosola@gmail.com>
2024-06-11 16:28:09 -0700

    Adding deploy scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+aws s3 sync --acl private build s3://bulls.yosola.co --delete --cache-control max-age=31536000,public
+aws s3 cp s3://bulls.yosola.co/index.html s3://bulls.yosola.co/index.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl private
